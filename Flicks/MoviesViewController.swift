a43797983651c9e75757968cf971958d2416eb13
Nhan Nguyen <nhannv.fu6b@gmail.com>
2016-07-10 22:10:02 +0700

    Polish UI

--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -199,7 +199,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
